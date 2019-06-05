84f45071dd81fcfb3316d8fbc65cf79306327fd1
RyanBurnside <pixeloutlaw@gmail.com>
2019-06-04 22:18:48 -0600

    Updated for review.

--- classes.lisp
+++ classes.lisp
@@ -1,5 +1,23 @@
 (cl:in-package :trivial-stg)
 
+;; Note, these objects are designed to work best for this demo.
+;; They may not be as complex, flexible, or reusable as they could be
+
+(defclass mover ()
+  ((center-position
+    :accessor center-position
+    :initarg :center-position
+    :initform (gamekit:vec2 0.0 0.0))
+   (velocity-vector
+    :accessor velocity-vector
+    :initarg :velocity-vector
+    :initform (gamekit:vec2 0.0 0.0))))
+
+(defgeneric update-object (instance))
+(defmethod update-object ((m mover))
+  (incf (x (center-position m)) (x (velocity-vector m)))
+  (incf (y (center-position m)) (y (velocity-vector m))))
+   
 (defclass hitbox ()
   ((dimensions
     :accessor dimensions
@@ -39,12 +57,22 @@
   (+ (gamekit:y (center-position h))
      (compute-half-height h)))
 
-(defgeneric collides-p (instance-a instance-b))
-(defmethod collides-p ((a hitbox) (b hitbox))
+(defgeneric collide-p (instance-a instance-b))
+(defmethod collide-p ((a hitbox) (b hitbox))
   (not
    (or
     (> (compute-top a) (compute-bottom b))
     (< (compute-bottom a) (compute-top b))
     (> (compute-left a) (compute-right b))
     (< (compute-right a) (compute-left b)))))
+
+;; For the sake of simplicty, game objects have 1 hitbox in this demo
+;; You may, in your project keep a collection but this is usually not necessary in shmups
+
+(defclass game-object (mover hitbox)
+  ((sprite
+    :accessor sprite
+    :initarg sprite
+    :initform null)))
+
 
--- trivial-stg.lisp
+++ trivial-stg.lisp
@@ -2,4 +2,35 @@
 
 (in-package #:trivial-stg)
 
-  
+(defparameter *background* (gamekit:vec4 0.2 0.2 0.2 1))
+(defparameter *foreground* (gamekit:vec4 0.8 0.8 0.8 1))
+(defparameter *player-shots* (list))
+(defparameter *enemy-shots* (list))
+
+(defparameter *cw* 240)
+(defparameter *ch* 320)
+
+
+(gamekit:defgame game () ()
+  (:viewport-width 480)
+  (:viewport-height 640)
+  (:canvas-width *cw*)
+  (:canvas-height *ch*)
+  (:viewport-title "Trivial-STG")
+  (:default-initargs 
+   ))
+
+;(gamekit:define-image :ship
+;    (asdf:system-relative-pathname :trivial-gamekit-postproc/example "./example/forthebenefitormrkite.png"))
+
+(defmethod gamekit:draw ((this game))
+  (gamekit:draw-text "Score" (gamekit:vec2 120 120))
+  (bodge-canvas:antialias-shapes nil)
+
+)
+
+
+(defun run ()
+  (gamekit:start 'game :swap-interval 0))
+
+(run)
